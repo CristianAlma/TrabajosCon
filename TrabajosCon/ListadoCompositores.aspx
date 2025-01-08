9cad3cf6e67907c94b70568c57a5f9ceb2a3423e
cristianalma <cristianeltem315@gmail.com>
2025-01-08 01:50:35 -0500

    ListadoCompoV1

---
+++ TrabajosCon/ListadoCompositores.aspx
@@ -0,0 +1,16 @@
+﻿<%@ Page Title="Listado de Compositores" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="ListadoCompositores.aspx.cs" Inherits="TrabajosCon.ListadoCompositores" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+    <div class="container mt-5">
+        <h2 class="text-center">Listado de Compositores</h2>
+        <div class="text-right mb-3">
+            <a href="compositores.aspx" class="btn btn-primary">Agregar Compositor</a>
+        </div>
+        <asp:GridView ID="gvCompositores" runat="server" CssClass="table table-bordered" AutoGenerateColumns="False">
+            <Columns>
+                <asp:BoundField DataField="Nombre" HeaderText="Nombre" />
+                <asp:BoundField DataField="Bibliografia" HeaderText="Bibliografía" />
+            </Columns>
+        </asp:GridView>
+    </div>
+</asp:Content>
---
+++ TrabajosCon/ListadoCompositores.aspx.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web.UI;
+
+namespace TrabajosCon
+{
+    public partial class ListadoCompositores : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                CargarCompositores();
+            }
+        }
+
+        private void CargarCompositores()
+        {
+            // Lista simulada
+            var compositores = new List<Compositor>
+            {
+                new Compositor { Nombre = "Ludwig van Beethoven", Bibliografia = "Uno de los más grandes compositores de la historia." },
+                new Compositor { Nombre = "Wolfgang Amadeus Mozart", Bibliografia = "Conocido por sus sinfonías y óperas." }
+            };
+
+            gvCompositores.DataSource = compositores;
+            gvCompositores.DataBind();
+        }
+
+        public class Compositor
+        {
+            public string Nombre { get; set; }
+            public string Bibliografia { get; set; }
+        }
+    }
+}
